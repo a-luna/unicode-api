701beabc51ce47787c58243c7a9a04a64cf9f39f
Aaron Luna <contact@aaronluna.dev>
2023-02-12 17:37:57 -0800

    refactor: :recycle: rename NamelessCharacterType to UnassignedCharacterType

--- app/schemas/enums/__init__.py
+++ app/schemas/enums/__init__.py
@@ -8,9 +8,9 @@ from app.schemas.enums.general_category import GeneralCategory
 from app.schemas.enums.hangul_syllable_type import HangulSyllableType
 from app.schemas.enums.joining_type import JoiningType
 from app.schemas.enums.line_break_type import LineBreakType
-from app.schemas.enums.nameless_character import NamelessCharacterType
 from app.schemas.enums.numeric_type import NumericType
 from app.schemas.enums.property_group import CharPropertyGroup
 from app.schemas.enums.script_code import ScriptCode
 from app.schemas.enums.triadic_logic import TriadicLogic
+from app.schemas.enums.unassigned_char_type import UnassignedCharacterType
 from app.schemas.enums.vertical_orientation_type import VerticalOrientationType
--- app/schemas/enums/unassigned_char_type.py
+++ app/schemas/enums/unassigned_char_type.py
@@ -1,11 +1,12 @@
 from enum import auto, IntEnum
 
 
-class NamelessCharacterType(IntEnum):
+class UnassignedCharacterType(IntEnum):
     NONCHARACTER = auto()
     SURROGATE = auto()
     PRIVATE_USE = auto()
     RESERVED = auto()
+    INVALID = auto()
 
     def __str__(self):
         return self.name.replace("_", "-").lower()
