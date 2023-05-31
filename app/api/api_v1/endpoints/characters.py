abe7871a73bad1942c711c5bdd6b4711c1ac089f
Aaron Luna <contact@aaronluna.dev>
2023-05-31 08:40:06 -0700

    refactor: :recycle: minor refactoring to reduce nesting in get_unicode_character_details function

--- app/api/api_v1/endpoints/characters.py
+++ app/api/api_v1/endpoints/characters.py
@@ -8,19 +8,19 @@ from app.api.api_v1.dependencies import (
     CharacterSearchParameters,
     DBSession,
     FilterParameters,
-    get_session,
     ListParameters,
-    parse_enum_values_from_parameter,
     UnicodeBlockQueryParamResolver,
+    get_session,
+    parse_enum_values_from_parameter,
 )
 from app.api.api_v1.pagination import paginate_search_results
 from app.core.config import settings
 from app.data.cache import cached_data
 from app.data.encoding import get_codepoint_string
 from app.docs.dependencies.custom_parameters import (
-    get_description_and_values_table_for_property_group,
     UNICODE_CHAR_EXAMPLES,
     UNICODE_CHAR_STRING_DESCRIPTION,
+    get_description_and_values_table_for_property_group,
 )
 from app.schemas.enums import CharPropertyGroup
 
@@ -104,10 +104,9 @@ def get_unicode_character_details(
 ):
     if show_props:
         result = parse_enum_values_from_parameter(CharPropertyGroup, "show_props", show_props)
-        if result.success:
-            prop_groups = result.value
-        else:
+        if result.failure:
             raise HTTPException(status_code=int(HTTPStatus.BAD_REQUEST), detail=result.error)
+        prop_groups = result.value
     else:
         prop_groups = None
     return [get_character_details(db_ctx, ord(char), prop_groups) for char in string]
