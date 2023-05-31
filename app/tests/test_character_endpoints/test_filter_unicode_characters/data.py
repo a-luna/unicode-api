64e8ae824448f5666bee3d641d3952b8e8b8e2cd
Aaron Luna <contact@aaronluna.dev>
2023-05-31 08:38:15 -0700

    test: :white_check_mark: add test coverage for ability to filter characters by decomp_type

--- app/tests/test_character_endpoints/test_filter_unicode_characters/data.py
+++ app/tests/test_character_endpoints/test_filter_unicode_characters/data.py
@@ -31,3 +31,30 @@ FILTER_BY_UNICODE_AGE = {
         {"character": "﯂", "name": "ARABIC SYMBOL WASLA ABOVE", "codepoint": "U+FBC2", "uriEncoded": "%EF%AF%82"},
     ],
 }
+
+FILTER_BY_BIDI_CLASS = {
+    "url": "/v1/characters/filter",
+    "hasMore": False,
+    "currentPage": 1,
+    "totalResults": 1,
+    "results": [{"character": "₫", "name": "DONG SIGN", "codepoint": "U+20AB", "uriEncoded": "%E2%82%AB"}],
+}
+
+FILTER_BY_DECOMP_TYPE = {
+    "url": "/v1/characters/filter",
+    "hasMore": False,
+    "currentPage": 1,
+    "totalResults": 6,
+    "results": [
+        {"character": "⑦", "name": "CIRCLED DIGIT SEVEN", "codepoint": "U+2466", "uriEncoded": "%E2%91%A6"},
+        {"character": "⑰", "name": "CIRCLED NUMBER SEVENTEEN", "codepoint": "U+2470", "uriEncoded": "%E2%91%B0"},
+        {"character": "㉗", "name": "CIRCLED NUMBER TWENTY SEVEN", "codepoint": "U+3257", "uriEncoded": "%E3%89%97"},
+        {"character": "㊆", "name": "CIRCLED IDEOGRAPH SEVEN", "codepoint": "U+3286", "uriEncoded": "%E3%8A%86"},
+        {"character": "㊲", "name": "CIRCLED NUMBER THIRTY SEVEN", "codepoint": "U+32B2", "uriEncoded": "%E3%8A%B2"},
+        {"character": "㊼", "name": "CIRCLED NUMBER FORTY SEVEN", "codepoint": "U+32BC", "uriEncoded": "%E3%8A%BC"},
+    ],
+}
+
+INVALID_FILTER_PARAM_VALUES = {
+    "detail": "Invalid values were provided for the following 6 parameters:\n\n2 values provided for the 'category' parameter are invalid: ['aa', 'bb']\n\n2 values provided for the 'age' parameter are invalid: ['7.1', '12.97']\n\n2 values provided for the 'script' parameter are invalid: ['blar', 'blee']\n\n2 values provided for the 'bidi_class' parameter are invalid: ['vv', 'rr']\n\n1 value provided for the 'decomp_type' parameter is invalid: ['gosh']\n\n2 values provided for the 'show_props' parameter are invalid: ['soup', 'salad']"
+}
--- app/tests/test_character_endpoints/test_filter_unicode_characters/test_filter_unicode_characters.py
+++ app/tests/test_character_endpoints/test_filter_unicode_characters/test_filter_unicode_characters.py
@@ -2,7 +2,10 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 from app.tests.test_character_endpoints.test_filter_unicode_characters.data import (
+    FILTER_BY_BIDI_CLASS,
+    FILTER_BY_DECOMP_TYPE,
     FILTER_BY_UNICODE_AGE,
+    INVALID_FILTER_PARAM_VALUES,
     INVALID_PAGE_NUMBER,
     NAME_SPIRITUS_CATEGORY_MN_SCRIPT_COPT,
     NO_CHARS_MATCH_SETTINGS,
@@ -24,7 +27,7 @@ def test_invalid_page_number():
 
 
 def test_no_characters_match_filter_settings():
-    response = client.get("/v1/characters/filter?name=test&script=copt&per_page=100&page=1")
+    response = client.get("/v1/characters/filter?name=test&script=copt&show_props=all")
     assert response.status_code == 200
     assert response.json() == NO_CHARS_MATCH_SETTINGS
 
@@ -33,3 +36,23 @@ def test_filter_by_unicode_age():
     response = client.get("/v1/characters/filter?category=sk&age=13.0&age=14.0&age=15.0")
     assert response.status_code == 200
     assert response.json() == FILTER_BY_UNICODE_AGE
+
+
+def test_filter_by_bidi_class():
+    response = client.get("/v1/characters/filter?name=dong&bidi_class=et")
+    assert response.status_code == 200
+    assert response.json() == FILTER_BY_BIDI_CLASS
+
+
+def test_filter_by_decomp_type():
+    response = client.get("/v1/characters/filter?name=seven&decomp_type=enc")
+    assert response.status_code == 200
+    assert response.json() == FILTER_BY_DECOMP_TYPE
+
+
+def test_invalid_filter_param_values():
+    response = client.get(
+        "/v1/characters/filter?category=aa&category=bb&age=7.1&age=12.97&script=blar&script=blee&bidi_class=vv&bidi_class=rr&show_props=soup&show_props=salad&decomp_type=gosh"
+    )
+    assert response.status_code == 400
+    assert response.json() == INVALID_FILTER_PARAM_VALUES
