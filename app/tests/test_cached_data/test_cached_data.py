94159e32989d09d678eee8074d34109f851139c2
Aaron Luna <contact@aaronluna.dev>
2023-06-19 04:46:18 -0700

    test: :white_check_mark: add test coverage for user request invalid codepoint (not in unicode range)

--- app/tests/test_cached_data/test_cached_data.py
+++ app/tests/test_cached_data/test_cached_data.py
@@ -5,3 +5,9 @@ TOTAL_CHARACTERS_IN_UNICODE_V15_0 = 149186
 
 def test_total_number_of_unicode_characters():
     assert cached_data.official_number_of_unicode_characters == TOTAL_CHARACTERS_IN_UNICODE_V15_0
+
+
+def test_get_char_name_invalid_codepoint():
+    codepoint = 1114112
+    char_name = cached_data.get_character_name(codepoint)
+    assert char_name == "Invalid Codepoint (U+110000)"
