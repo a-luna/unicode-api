70073795ef345f06c128715ea99a6c08e7940edf
Aaron Luna <contact@aaronluna.dev>
2023-06-22 20:07:51 -0700

    test: :white_check_mark: add test coverage for verbosity parameter

--- app/tests/test_character_endpoints/test_get_unicode_character_details/test_get_character_details.py
+++ app/tests/test_character_endpoints/test_get_unicode_character_details/test_get_character_details.py
@@ -12,22 +12,23 @@ from app.db.get_char_details import get_prop_groups
 from app.main import app
 from app.schemas.enums import CharPropertyGroup
 from app.tests.test_character_endpoints.test_get_unicode_character_details.data import (
-    ALL_CHARACTER_PROPERTIES,
     ALL_PROP_GROUP_NAMES,
+    CHARACTER_PROPERTIES,
     INVALID_PROP_GROUP_NAMES,
+    VERBOSE_CHARACTER_PROPERTIES,
 )
 
 client = TestClient(app)
 
 
-def get_character_properties(char, prop_group):
-    prop_data = ALL_CHARACTER_PROPERTIES[char]
+def get_character_properties(char, prop_group, verbose=False):
     prop_groups = get_prop_groups(ord(char), [prop_group])
-    char_prop_dicts = [get_prop_group(group, prop_data) for group in prop_groups]
+    char_prop_dicts = [get_prop_group(char, group, verbose) for group in prop_groups]
     return reduce(operator.ior, char_prop_dicts, {})
 
 
-def get_prop_group(prop_group, prop_data):
+def get_prop_group(char, prop_group, verbose):
+    prop_data = CHARACTER_PROPERTIES[char] if not verbose else VERBOSE_CHARACTER_PROPERTIES[char]
     return {
         camelize(prop_details["name_out"]): prop_data[camelize(prop_details["name_out"])]
         for prop_details in PROPERTY_GROUPS[prop_group]
@@ -35,7 +36,7 @@ def get_prop_group(prop_group, prop_data):
     }
 
 
-@pytest.mark.parametrize("char", ALL_CHARACTER_PROPERTIES.keys())
+@pytest.mark.parametrize("char", CHARACTER_PROPERTIES.keys())
 def test_get_character_details_default(char):
     url = f"/v1/characters/{char}"
     if any(char.isascii() and not char.isprintable() for char in url):
@@ -45,18 +46,19 @@ def test_get_character_details_default(char):
     )
     response = client.get(url)
     assert response.status_code == 200
-    assert response.json() == [get_character_properties(char, prop_group)]
+    assert response.json() == [get_character_properties(char, prop_group, False)]
 
 
-@pytest.mark.parametrize("char", ALL_CHARACTER_PROPERTIES.keys())
+@pytest.mark.parametrize("q_verbose, verbose", [("&verbose=true", True), ("&verbose=false", False), ("", None)])
+@pytest.mark.parametrize("char", CHARACTER_PROPERTIES.keys())
 @pytest.mark.parametrize("prop_group", ALL_PROP_GROUP_NAMES)
-def test_get_character_details_show_props(char, prop_group):
-    url = f"/v1/characters/{char}?show_props={prop_group}"
+def test_get_character_details_show_props(q_verbose, verbose, char, prop_group):
+    url = f"/v1/characters/{char}?show_props={prop_group}{q_verbose}"
     if any(char.isascii() and not char.isprintable() for char in url):
-        url = f"/v1/characters/{get_uri_encoded_value(char)}?show_props={prop_group}"
+        url = f"/v1/characters/{get_uri_encoded_value(char)}?show_props={prop_group}{q_verbose}"
     response = client.get(url)
     assert response.status_code == 200
-    assert response.json() == [get_character_properties(char, CharPropertyGroup.match_loosely(prop_group))]
+    assert response.json() == [get_character_properties(char, CharPropertyGroup.match_loosely(prop_group), verbose)]
 
 
 def test_invalid_prop_group_name():
