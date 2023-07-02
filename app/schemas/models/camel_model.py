6e547a724cec5fff49889749ed09e52c7a39d8af
Aaron Luna <contact@aaronluna.dev>
2023-07-01 17:46:29 -0700

    feat: :sparkles: create custom alias generator to convert response property names to lower camel case

--- app/schemas/models/camel_model.py
+++ app/schemas/models/camel_model.py
@@ -1,19 +1,16 @@
-from humps import camelize
 from pydantic.generics import GenericModel
 from sqlmodel import SQLModel
 
-
-def to_camel(string):
-    return camelize(string)
+from app.schemas.util import to_lower_camel
 
 
 class CamelModel(SQLModel):
     class Config:
-        alias_generator = to_camel
+        alias_generator = to_lower_camel
         allow_population_by_field_name = True
 
 
 class GenericCamelModel(GenericModel):
     class Config:
-        alias_generator = to_camel
+        alias_generator = to_lower_camel
         allow_population_by_field_name = True
--- app/schemas/util.py
+++ app/schemas/util.py
@@ -1,5 +1,7 @@
 import re
 
+UNDERSCORE_RE = re.compile(r"(?<=[^\-_])[\-_]+[^\-_]")
+
 
 def normalize_string_lm3(s: str) -> str:
     # https://www.unicode.org/reports/tr44/#UAX44-LM3
@@ -7,3 +9,12 @@ def normalize_string_lm3(s: str) -> str:
     if s.startswith("is") and len(s) > 2:  # pragma: no cover
         s = s[2:]
     return re.compile(r"\s+").sub("", s).replace("-", "").replace("_", "")
+
+
+def to_lower_camel(input):
+    s = "" if input is None else re.sub(r"\s+", "", str(input))
+    if s.isupper() or s.isnumeric():
+        return input
+    if len(s) != 0 and not s[:2].isupper():
+        s = s[0].lower() + s[1:]
+    return UNDERSCORE_RE.sub(lambda m: m.group(0)[-1].upper(), s)
