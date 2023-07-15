2f3256128fbd8f8c10dd30de3f44f63624677d88
Aaron Luna <contact@aaronluna.dev>
2023-07-15 16:11:15 -0700

    chore: :truck: reorganize folder structure of unicode version data files

--- app/core/config.py
+++ app/core/config.py
@@ -16,11 +16,13 @@ XML_ZIP_FILE_NAME = "ucd.all.flat.zip"
 DB_FILE_NAME = "unicode-api.db"
 DB_ZIP_FILE_NAME = "unicode-api.db.zip"
 
-ROOT_FOLDER = Path(__file__).parent.parent.parent
+APP_FOLDER = Path(__file__).parent.parent
+ROOT_FOLDER = APP_FOLDER.parent
 DOTENV_FILE = ROOT_FOLDER.joinpath(".env")
 
-if os.environ.get("ENV", "") != "PROD":
+if os.environ.get("ENV", "") != "PROD":  # pragma: no cover
     dotenv = DotEnvFile(dotenv_filepath=DOTENV_FILE)
+
 
 class UnicodeApiSettings(BaseSettings):
     ENV: str = os.environ.get("ENV", "DEV")
@@ -38,21 +40,22 @@ class UnicodeApiSettings(BaseSettings):
     ROOT_FOLDER: Path = ROOT_FOLDER
     APP_FOLDER: Path = ROOT_FOLDER.joinpath("app")
     DATA_FOLDER: Path = APP_FOLDER.joinpath("data")
-    XML_FOLDER: Path = DATA_FOLDER.joinpath("xml").joinpath(UNICODE_VERSION)
+    VERSION_FOLDER: Path = DATA_FOLDER.joinpath("unicode_versions").joinpath(UNICODE_VERSION)
+    XML_FOLDER: Path = VERSION_FOLDER.joinpath("xml")
     XML_FILE: Path = XML_FOLDER.joinpath(XML_FILE_NAME)
     XML_ZIP_FILE: Path = XML_FOLDER.joinpath(XML_ZIP_FILE_NAME)
-    DB_FOLDER: Path = DATA_FOLDER.joinpath("db").joinpath(UNICODE_VERSION)
+    DB_FOLDER: Path = VERSION_FOLDER.joinpath("db")
     DB_FILE: Path = DB_FOLDER.joinpath(DB_FILE_NAME)
     DB_ZIP_FILE: Path = DB_FOLDER.joinpath(DB_ZIP_FILE_NAME)
     DB_ZIP_URL: str = f"{HTTP_BUCKET_URL}/{UNICODE_VERSION}/{DB_ZIP_FILE.name}"
     DB_URL: str = f"sqlite:///{DB_FILE}"
-    JSON_FOLDER: Path = DATA_FOLDER.joinpath("json").joinpath(UNICODE_VERSION)
+    JSON_FOLDER: Path = VERSION_FOLDER.joinpath("json")
     PLANES_JSON: Path = JSON_FOLDER.joinpath("planes.json")
     BLOCKS_JSON: Path = JSON_FOLDER.joinpath("blocks.json")
     CHAR_NAME_MAP: Path = JSON_FOLDER.joinpath("char_name_map.json")
     JSON_ZIP_FILE: Path = JSON_FOLDER.joinpath("unicode_json.zip")
     JSON_ZIP_URL: str = f"{HTTP_BUCKET_URL}/{UNICODE_VERSION}/{JSON_ZIP_FILE.name}"
-    CSV_FOLDER: Path = DATA_FOLDER.joinpath("csv").joinpath(UNICODE_VERSION)
+    CSV_FOLDER: Path = VERSION_FOLDER.joinpath("csv")
     PLANES_CSV: Path = CSV_FOLDER.joinpath("planes.csv")
     BLOCKS_CSV: Path = CSV_FOLDER.joinpath("blocks.csv")
     NAMED_CHARS_CSV: Path = CSV_FOLDER.joinpath("named_chars.csv")
