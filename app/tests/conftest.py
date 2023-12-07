5352fff176b4afc987f62bbb910a68a12e3bf1bb
Aaron Luna <contact@aaronluna.dev>
2023-12-07 04:10:03 -0800

    test: :white_check_mark: add TEST_HEADER environment variable to test fixture

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -7,6 +7,7 @@ from fastapi.testclient import TestClient
 @pytest.fixture(scope="function")
 def client(request):
     os.environ["ENV"] = "TEST"
+    os.environ["TEST_HEADER"] = "x-unicodeapi-test"
     from app.main import app
 
     with TestClient(app) as client:
