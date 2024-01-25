d546b91376960102f9c2bb2cb08f07030b367b50
Aaron Luna <contact@aaronluna.dev>
2024-01-24 16:51:06 -0800

    refactor: :recycle: refactor check_min_version to accept a version parameter

--- app/data/scripts/bootstrap_unicode_data.py
+++ app/data/scripts/bootstrap_unicode_data.py
@@ -9,8 +9,8 @@ SEMVER_REGEX = re.compile(r"^(?P<major>(?:[1-9]\d*))\.(?P<minor>(?:[0-9]\d*))(?:
 
 
 def bootstrap_unicode_data() -> Result[UnicodeApiSettings]:
-    if os.environ.get("UNICODE_VERSION"):
-        result = check_min_version()
+    if version := os.environ.get("UNICODE_VERSION"):
+        result = check_min_version(version)
         if result.failure:
             return Result.Fail(result.error if result.error else "")
     else:
@@ -21,12 +21,12 @@ def bootstrap_unicode_data() -> Result[UnicodeApiSettings]:
     return Result.Ok(config)
 
 
-def check_min_version() -> Result[None]:
-    check_version = os.environ.get("UNICODE_VERSION", "0")
-    match = SEMVER_REGEX.match(check_version)
-    if not match:
-        return Result.Fail(f"'{check_version}' is not a valid semantic version number")
-    if check_version in SUPPORTED_UNICODE_VERSIONS:
+def check_min_version(check_version: str) -> Result[None]:
+    result = parse_semver_string(check_version)
+    if result.failure:
+        return Result.Fail(result.error)
+    (major, minor, patch) = result.value
+    if f"{major}.{minor}.{patch}" in SUPPORTED_UNICODE_VERSIONS:
         return Result.Ok()
     error = (
         "This script parses the XML representation of the Unicode Character Database, which has been distributed "
@@ -36,3 +36,14 @@ def check_min_version() -> Result[None]:
         f"the following valid version numbers: {', '.join(SUPPORTED_UNICODE_VERSIONS)}"
     )
     return Result.Fail(error)
+
+
+def parse_semver_string(input: str) -> Result[tuple[int, int, int]]:
+    match = SEMVER_REGEX.match(input)
+    if not match:
+        return Result.Fail(f"'{input}' is not a valid semantic version")
+    groups = match.groupdict()
+    major = groups.get("major", "0")
+    minor = groups.get("minor", "0")
+    patch = groups.get("patch", "0")
+    return Result.Ok((int(major), int(minor), int(patch)))
