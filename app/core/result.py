a8815f38be46f148b9040879eb172cc4ad96b28e
Aaron Luna <contact@aaronluna.dev>
2024-05-04 06:22:02 -0700

    chore: :label: improve type references for app.core.result module

--- app/core/result.py
+++ app/core/result.py
@@ -1,12 +1,8 @@
-from __future__ import annotations
-
 from collections.abc import Callable
-from typing import Generic, TypeVar
-
-T = TypeVar("T")
+from typing import Self, overload
 
 
-class Result(Generic[T]):
+class Result[T]:
     """Represent the outcome of an operation."""
 
     def __init__(self, success: bool, value: T | None, error: str | None) -> None:
@@ -22,32 +18,46 @@ class Result(Generic[T]):
 
     def __repr__(self) -> str:
         """Official string representation of a result."""
-        detail = f", error={self.error!r}" if self.failure else f", value={self.value}" if self.value else ""
-        return f"<Result success={self.success}{detail}>"
+        detail = (
+            f"value=None, error={self.error!r}"
+            if self.failure
+            else f"value={self.value!r}, error=None"
+            if self.value
+            else "value=None, error=None"
+        )
+        return f"Result({"True" if self.success else "False"}, {detail})"
 
     @property
     def failure(self) -> bool:
         """Flag that indicates if the operation failed."""
         return not self.success
 
-    def on_success(self, func: Callable, *args, **kwargs) -> Result[T]:
+    def on_success(self, func: Callable, *args, **kwargs) -> Self:
         """Pass result of successful operation (if any) to subsequent function."""
         return self if self.failure else func(self.value, *args, **kwargs) if self.value else func(*args, **kwargs)
 
-    def on_failure(self, func: Callable, *args, **kwargs) -> Result[T]:
+    def on_failure(self, func: Callable, *args, **kwargs) -> Self:
         """Pass error message from failed operation to subsequent function."""
         return self if self.success else func(self.error, *args, **kwargs)
 
-    def on_both(self, func: Callable, *args, **kwargs) -> Result[T]:
+    def on_both(self, func: Callable, *args, **kwargs) -> Self:
         """Pass result (either succeeded/failed) to subsequent function."""
         return func(self, *args, **kwargs)
 
     @staticmethod
-    def Fail(error_message: str) -> Result[T]:  # noqa: N802
+    def Fail(error_message: str) -> Self:  # noqa: N802
         """Create a Result object for a failed operation."""
         return Result(False, value=None, error=error_message)
 
+    @overload
     @staticmethod
-    def Ok(value: T | None = None) -> Result[T]:  # noqa: N802
+    def Ok() -> "Result": ...
+
+    @overload
+    @staticmethod
+    def Ok(value: T) -> "Result[T]": ...
+
+    @staticmethod
+    def Ok(value: T | None = None) -> "Result | Result[T]":  # noqa: N802
         """Create a Result object for a successful operation."""
         return Result(True, value=value, error=None)
